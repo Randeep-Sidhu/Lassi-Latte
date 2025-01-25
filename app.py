3d5d3a156c1ac1e1c023647a8332d1743ebaea49
Randeep Sidhu <randeepssstar@gmail.com>
2025-01-25 11:51:36 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/account')
+def account():
+    return render_template('account.html')
+
+@app.route('/basket')
+def basket():
+    return render_template('basket.html')
+
+@app.route('/careers')
+def careers():
+    return render_template('careers.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/feedback')
+def feedback():
+    return render_template('feedback.html')
+
+@app.route('/terms')
+def terms():
+    return render_template('terms.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
